578cc5fa1891c92b6e4fe5064e58122ac3f21a0f
HiromasaWatanabe <banflorence@icloud.com>
2019-12-16 17:37:07 +0900

    コードを読みやすくするために調整

--- db/migrate/20191216073511_create_items.rb
+++ db/migrate/20191216073511_create_items.rb
@@ -1,15 +1,15 @@
 class CreateItems < ActiveRecord::Migration[5.2]
   def change
     create_table :items do |t|
-      t.string :item, null: false
-      t.text :description, null: false
-      t.integer :price, null:false
-      t.integer :shopping_status, null:false
-      t.integer :user_id, null:false, foreign_key: true
-      t.integer :category_id, null:false, foreign_key: true
-      t.integer :brands_category_id, foreign_key: true
-      t.integer :size_id, null:false, foreign_key: true
-      t.integer :item_condition_id, null:false, foreign_key: true
+      t.string  :item,              null: false
+      t.text    :description,       null: false
+      t.integer :price,             null: false
+      t.integer :shopping_status,   null: false
+      t.integer :user_id,           null: false,  foreign_key: true
+      t.integer :category_id,       null: false,  foreign_key: true
+      t.integer :brands_category_id,              foreign_key: true
+      t.integer :size_id,           null: false,  foreign_key: true
+      t.integer :item_condition_id, null: false,  foreign_key: true
       t.timestamps
     end
   end
