ac3076358b0e72fd3f759299c282231cd0041412
HiromasaWatanabe <banflorence@icloud.com>
2019-12-16 19:10:04 +0900

    itemテーブルのテーブル間の紐付け

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,9 @@
 class Item < ApplicationRecord
+  has_many :images, dependent: :destroy
+
+  belongs_to :user
+  belongs_to :category
+  belongs_to :brands_category
+  belongs_to :size
+  belongs_to :item_condition
 end
