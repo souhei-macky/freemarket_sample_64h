c77d5a215919f6c7fcc7a8d6ff7dbfec51883d3e
souhei-macky <souhei515151@gmail.com>
2019-12-17 21:22:03 +0900

    検索機能level1 機能のみ実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,4 +7,15 @@ class ItemsController < ApplicationController
     
   end
 
+  def search
+    # like句によるあいまい検索
+    @items = Item.where('item LIKE(?)', "%#{params[:keyword]}%").order("id DESC")
+    if params[:keyword] == ""
+      redirect_to '/items/search?utf8=✓&keyword=+++'
+    end
+    if @items.count == 0
+      @all_items = Item.limit(5).order("id DESC")
+    end
+  end
+
 end
