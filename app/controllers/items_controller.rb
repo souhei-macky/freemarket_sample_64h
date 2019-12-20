b4cd4ee03990f1ac8735e6326711ce9238a60bc1
Shin <5.1.1.cn7568m325373c@gmail.com>
2019-12-20 14:27:19 +0900

    items_controller.rb_edited
    binding.pryを削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,7 +6,6 @@ class ItemsController < ApplicationController
   end
 
   def create
-    binding.pry
     @item = Item.new(item_params)
     if @item.save
       params[:images][:image].each do |image|
