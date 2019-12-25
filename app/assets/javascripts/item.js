d0f6c3aae76ad1425bcf9ff2721c9107ffc4f40f
Shin <5.1.1.cn7568m325373c@gmail.com>
2019-12-25 17:08:38 +0900

    edited_done
    プレビューの表示形式を変更し、11個目以降は表示させない記述に変更

--- app/assets/javascripts/item.js
+++ app/assets/javascripts/item.js
@@ -1,5 +1,4 @@
-// turbolinksを先に読み込む
-$(document).on('turbolinks:load', function() {
+$(function() {
   // 画像を管理するための配列を定義する。
   var images = [];
   // htmlを定義
@@ -9,23 +8,23 @@ $(document).on('turbolinks:load', function() {
                     <img src="${imgSrc}", height=118, width=118, class="image">
                   </div>
                   <div class="preview-box__select">
-                      <p>削除</p>
+                    <p>削除</p>
                   </div>
                 </div>`
     // .sell-dropbox-containerの配列の最初に追加
-    $('.sell-dropbox-container').prepend(html);
+    $('.sell-upload-items-container').before(html);
   };
 
   // アップロードボタンの表示
   function CountPreview() {
     // 画像が10枚の時、アップロードボタンを非表示
-    if ($('.preview-box__select p').length == 10) {
+    if ($('.preview-box').length == 10) {
       $('.sell-upload-drop-box').css('display', 'none');
     };
   };
 
   // 画像を入れた時にイベント発火
-  $('.fileup_last').on("change", function(e){
+  $(document).on('change', '.fileup,.fileup_last', function(e){
     // イベント発火したファイルを定義
     var files = e.target.files;
     // 画像のファイルを一つづつ、先ほどの画像管理用の配列に追加する。
@@ -38,48 +37,47 @@ $(document).on('turbolinks:load', function() {
         // htmlの呼び出し
         buildHTML(imgSrc);
         images.push(imgSrc);
-        if ($('.preview-box__select p').length > 10) {
+        if ($('.preview-box').length > 10) {
           alert('画像は最大10枚です')
           // 11枚以降のプレビューを全削除
-          $('.preview-box').slice(10,).remove();
+          $('.preview-box').slice(10).remove();
         };
         CountPreview();
       }
     }
   });
-  // 削除ボタンを押すと発火
+  // 黒い削除ボタン(DB未保存の画像)を押すと発火
   $(document).on('click', '.preview-box__select p', function(){
-  // 商品投稿ページの場合
-    if (window.location.href.match(/items\/new/)){
     // 画像削除を容認
     $(this).closest('.preview-box').remove();
-    } else {
-    // 商品編集ページの場合
+
+    if ($('.preview-box').length < 10) {
+      $('.sell-upload-drop-box').css('display', 'block');
+    };
+  });
+  // 赤い削除ボタン(DB保存済の画像)を押すと発火
+  $(document).on('click', '.preview-box__select-2 p', function(){
     if (window.location.href.match(/items\/\d+\/edit/)){
-      // 画像が残り1枚の場合
-      if ($('.preview-box__select p').length == 1) {
+      // DB保存済み画像が残り1枚の場合
+      if ($('.preview-box__select-2').length == 1) {
         alert('削除できません');
         //画像削除を無効化
         return false;
       } else {
         // 画像削除を容認
         $(this).closest('.preview-box').remove();
+        CountPreview();
       };
     };
-    };
-    if ($('.preview-box__select p').length < 10) {
-      $('.sell-upload-drop-box').css('display', 'block');
-    };
+  });
+// 画像投稿をする度にinputを追加する記述
+  $(document).on('change', '.fileup,.fileup_last',function(event) {
+    let input_area = $('.sell-upload-drop-box');
+    let new_input = $(`<input multiple= "multiple" name="images[image][]" class="boxfileselect fileup_last" type="file" accept="image/*">`);
+    input_area.prepend(new_input);
+    input_area.children(":first").css({'display':'block'});
+    // 選択したインプットは消す。
+    $(this).css({'display':'none'});    
   });
 
-// 画像投稿をする度にinputを追加する記述
-  let input_area = $('.sell-upload-drop-box');
-  $(document).on('change', '.fileup,.fileup_last',function(event) {
-    var new_input = $(`<input multiple= "multiple" name="images[image][]" class="boxfileselect fileup" type="file" accept="image/*">`);
-    input_area.prepend(new_input);
-  });
-  
-  if (window.location.href.match(/items\/\d+\/edit/)){
-    CountPreview();
-  };
 });
